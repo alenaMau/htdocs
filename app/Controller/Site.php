797e73315ba5e5448e1820db1714633d3e5230e8
AlenaMau <alenamau3486@gmail.com>
2024-03-01 22:26:12 +0700

    2 laba init

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -2,24 +2,88 @@
 
 namespace Controller;
 
+use Illuminate\Database\Capsule\Manager as DB;
 use Model\Post;
+use Src\Request;
 use Src\View;
+use Model\User;
+use Src\Auth\Auth;
+use Src\Validator\Validator;
+
 
 class Site
 {
-   public function index(): string
-   {
-       $posts = Post::all();
+    public function index(Request $request): string
+    {
+       $posts = Post::where('id', $request->id)->get();
        return (new View())->render('site.post', ['posts' => $posts]);
-   }
-
-   public function main(): string 
-   {
-    die("Проба");
-   }
+    }
+    
    public function hello(): string
    {
        return new View('site.hello', ['message' => 'hello working']);
-   }   
+   }  
+   
+   public function login(Request $request): string
+   {
+       if ($request->method === 'POST') {
+           $validator = new Validator($request->all(), [
+               'login' => ['required'],
+               'password' => ['required'],
+           ], [
+               'required' => 'Поле :field пусто',
+               'unique' => 'Поле :field должно быть уникально',
+               'russian' => 'Поле :field должно содержать только русский алфавит',
+               'number' => 'Поле :field должно содержать только цифры',
+               'not_number' => 'Поле :field должно содержать только буквы'
+           ]);
+
+           if ($validator->fails()) {
+               return new View('site.login',
+                   ['message' => json_encode($validator->errors(), JSON_UNESCAPED_UNICODE)]);
+           }
+       }
+
+       if ($request->method === 'GET') {
+           return new View('site.login');
+       }
+
+       if (Auth::attempt($request->all())) {
+           app()->route->redirect('/hello');
+       }
+       //Если аутентификация не удалась, то сообщение об ошибке
+       return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+   }
+
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
+    }
+
+    public function signup(Request $request): string
+    {
+   if ($request->method === 'POST') {
+
+       $validator = new Validator($request->all(), [
+           'name' => ['required'],
+           'login' => ['required', 'unique:users,login'],
+           'password' => ['required']
+       ], [
+           'required' => 'Поле :field пусто',
+           'unique' => 'Поле :field должно быть уникально'
+       ]);
+
+       if($validator->fails()){
+           return new View('site.signup',
+               ['message' => json_encode($validator->errors(), JSON_UNESCAPED_UNICODE)]);
+       }
+
+       if (User::signup($request->all())) {
+           app()->route->redirect('/login');
+       }
+   }
+   return new View('site.signup');
+}
 
 }
---
+++ app/Model/User.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Src\Auth\IdentityInterface;
+
+
+class User extends Model implements IdentityInterface
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    protected $fillable = [
+        'name',
+        'login',
+        'password'
+    ];
+
+    protected static function booted()
+    {
+        static::created(function ($user) {
+            $user->password = md5($user->password);
+            $user->save();
+        });
+    }
+ 
+    public static function signup(array $data):self
+    {
+        $user = self::create($data);
+        $user->password = md5($user->password);
+        $user->save();
+        return $user;
+    }
+
+    public function findIdentity(int $id)
+    {
+        return self::where('id', $id)->first();
+    }
+
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function attemptIdentity(array $credentials)
+    {
+        return self::where(['login' => $credentials['login'],
+            'password' => md5($credentials['password'])])->first();
+    }
+
+}
